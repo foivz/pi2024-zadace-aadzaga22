4789e8239cd70616ed2d573789f69babda7f749f
Antonijo Adžaga <aadzaga22@foi.hr>
2024-05-30 13:59:09 +0200

    Kreiran repozitorij : EmployeeRepository

---
+++ Software/EcoRecycle Manager/EcoRecycle Manager/Repositories/EmployeeRepository.cs
@@ -0,0 +1,73 @@
+﻿using DBLayer;
+using EcoRecycle_Manager.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcoRecycle_Manager.Repositories
+{
+    public class EmployeeRepository
+    {
+        public static Employee GetEmployee(int employeeID)
+        {
+            Employee employee = null;
+
+            string sql = $"SELECT * FROM [dbo].[Employee] WHERE EmployeeID = {employeeID}";
+            DB.OpenConnection();
+            var reader = DB.GetDataReader(sql);
+            if (reader.HasRows)
+            {
+                reader.Read();
+                employee = CreateObject(reader);
+                reader.Close();
+            }
+
+            DB.CloseConnection();
+            return employee;
+        }
+
+        public static List<Employee> GetEmployees()
+        {
+            var employees = new List<Employee>();
+
+            string sql = "SELECT * FROM [dbo].[Employee]";
+            DB.OpenConnection();
+            var reader = DB.GetDataReader(sql);
+            while (reader.Read())
+            {
+                Employee employee = CreateObject(reader);
+                employees.Add(employee);
+            }
+
+            reader.Close();
+            DB.CloseConnection();
+
+            return employees;
+        }
+
+        private static Employee CreateObject(SqlDataReader reader)
+        {
+            int employeeID = int.Parse(reader["EmployeeID"].ToString());
+            string name = reader["Name"].ToString();
+            string surname = reader["Surname"].ToString();
+            string position = reader["Position"].ToString();
+            string contactNumber = reader["ContactNumber"].ToString();
+            string email = reader["Email"].ToString();
+
+            var employee = new Employee
+            {
+                EmployeeID = employeeID,
+                Name = name,
+                Surname = surname,
+                Position = position,
+                ContactNumber = contactNumber,
+                Email = email
+            };
+
+            return employee;
+        }
+    }
+}
