be4cc75fa9810f15e9d9516e3c0b524b24d4a545
Antonijo Adžaga <aadzaga22@foi.hr>
2024-05-30 13:32:55 +0200

    Kreirana klasa Customer

---
+++ Software/EcoRecycle Manager/EcoRecycle Manager/Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcoRecycle_Manager.Models
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        public override string ToString()
+        {
+            return $"Customer ID: {CustomerID}, Name: {Name}, Surname: {Surname}, Address: {Address}, Phone Number: {PhoneNumber}, Email: {Email}";
+        }
+    }
+}
