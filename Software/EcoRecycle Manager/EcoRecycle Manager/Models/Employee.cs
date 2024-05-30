e411b12f579bf3b878887e1a0dc3c676b49e8e39
Antonijo Adžaga <aadzaga22@foi.hr>
2024-05-30 13:32:00 +0200

    Kreirana klasa Employee

---
+++ Software/EcoRecycle Manager/EcoRecycle Manager/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcoRecycle_Manager.Models
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Position { get; set; }
+        public string ContactNumber { get; set; }
+        public string Email { get; set; }
+
+        public override string ToString()
+        {
+            return $"Employee ID: {EmployeeID}, Name: {Name}, Surname: {Surname}, Position: {Position}, Contact Number: {ContactNumber}, Email: {Email}";
+        }
+    }
+}
