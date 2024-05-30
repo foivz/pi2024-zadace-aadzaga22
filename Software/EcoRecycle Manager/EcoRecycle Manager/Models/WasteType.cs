dc2b7723dc57904b598dbb6276b106463bfc1ba0
Antonijo Adžaga <aadzaga22@foi.hr>
2024-05-30 13:30:56 +0200

    Kreirana klasa WasteType

---
+++ Software/EcoRecycle Manager/EcoRecycle Manager/Models/WasteType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcoRecycle_Manager.Models
+{
+    public class WasteType
+    {
+        public int WasteTypeID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public override string ToString()
+        {
+            return $"Waste Type ID: {WasteTypeID}, Name: {Name}, Description: {Description}";
+        }
+    }
+}
